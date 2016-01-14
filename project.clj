3db926f4a87e85bbbf255a7e6203ef80c68ff666
Andrew Cheng <andrew.cheng@panasonic.aero>
2016-01-14 12:29:29 -0800

    cljs 1.7.228 has parallel builds; del java 1.5

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [potemkin "0.3.9"]
                  [proteus "0.1.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  [org.clojure/clojurescript "1.7.170"]
+                                  [org.clojure/clojurescript "1.7.228"]
                                   [org.clojure/test.check "0.5.9"]
                                   [criterium "0.4.3"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]
@@ -21,7 +21,6 @@
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ^:replace ["-server" "-Xmx2g"]
   :java-source-paths ["src"]
-  :javac-options ["-target" "1.5" "-source" "1.5"]
   :jar-exclusions [#"\.cljx|\.DS_Store"]
   :source-paths ["src" "target/src" "target/classes"]
   :test-paths ["test" "target/test"]
@@ -45,7 +44,8 @@
                         :compiler {:output-to "target/test.js"
                                    :source-map "target/test.js.map"
                                    :output-dir "target/js"
-                                   :optimizations :advanced}}]
+                                   :optimizations :advanced
+                                   :parallel-build true}}]
               :test-commands {"phantom" ["phantomjs" :runner "target/test.js"]}}
   :codox {:writer codox-md.writer/write-docs
           :include [automat.core automat.viz]})
